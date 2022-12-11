f05a021d8d3d119d79243a9b38adab30c94bdd4c
Indra Wahjoedi <indra.wahjoedi@gmail.com>
2022-12-11 07:10:12 +0000

    add update container

--- update-bagisto.sh
+++ update-bagisto.sh
@@ -1,22 +1,32 @@
-## Update Bagisto Only
+# Bagisto Account
+# account: 
+# admin@admin@example.com:admin123
 
-compose_file='dev-apache-docker-compose.yml'
-gitpod_80=$(gp url 80)  
-sed -i "/APP_URL/c\APP_URL=${gitpod_80}" ".configs/.env"
+# container id by image name
+apache_container_id=$(cat apache_container_id)
+
+# update bagisto
+# echo "Now, setting up Bagisto..."
+# docker exec ${apache_container_id} git clone https://github.com/bagisto/bagisto
+docker exec -i ${apache_container_id} bash -c "rm -rf bagisto"
+docker cp ../bagisto ${apache_container_id}:/var/www/html/
 
 
-## Copying
-docker cp ../bagisto $(cat apache_container_id):/var/www/html/
-docker cp ../laravel-pwa $(cat apache_container_id):/var/www/html/
+# moving `.env` file
+docker cp .configs/.env ${apache_container_id}:/var/www/html/bagisto/.env
 
-# installing composer dependencies inside container
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && composer install"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan optimize:clear"
-# docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan migrate:fresh --seed"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan storage:link"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan bagisto:publish --force"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan config:cache"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan migrate"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan route:cache"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan vendor:publish --all"
-docker exec -i $(cat apache_container_id) bash -c "cd bagisto && php artisan optimize:clear"
+# executing final commands
+## Bagisto
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan optimize:clear && php artisan migrate:fresh --seed && php artisan storage:link && php artisan bagisto:publish --force && php artisan optimize:clear"
+docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan optimize:clear"
+docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan migrate:fresh --seed"
+docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan storage:link"
+docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan bagisto:publish --force"
+docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan optimize:clear"
+
+## Bagisto PWA
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && composer require bagisto/pwa:dev-master"
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan config:cache"
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan migrate"
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan route:cache"
+# docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan vendor:publish --all"
